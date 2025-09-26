e67473389f4069325c61cd4cf908dbd8ad148d31
Rafa1911 <rafael1911.menendez@gmail.com>
2025-09-25 22:04:22 -0600

    Commit semana 3

---
+++ Semana_3/Clase_3.9_(Jueves).py
@@ -0,0 +1,33 @@
+year = 2025
+period = 5 
+
+grad = year + period 
+nineth = year - 3
+salary = year * 0.25
+test = year / 3
+
+
+
+compa1 = year > nineth #El Alvin profe cree que si (Es CINE)
+compa2 = year < nineth #El Alvin profe cree que no (Un sabio)
+compa3 = compa1 + 10
+compa4 = compa2 + 10
+
+"""
+
+print(grad)
+print(nineth)
+print(salary)
+print(test)
+print(compa1)
+print(compa2)
+print(compa3)
+print(compa4)
+"""
+
+pi = 3.141516
+print(pi)
+pi_int = int(pi)
+
+pi_dec = pi - pi_int
+print(pi_dec)
---
+++ Semana_3/Mi_Primer_Script.py
@@ -0,0 +1,9 @@
+# Este programa fue hecho en Python 3.13.7 😤
+var1 = 100 # Son las ventas de enero
+var2 = 200
+var3 = var1 + var2
+print("Hello World")
+print(var3)
+""" En algunos casos, vamos a necesitar
+crear comentarios "miltilinea", de tal forma que
+nuestro yo del futuro sepa que hicimos y porque ...CINE👈(ﾟヮﾟ👈)"""
