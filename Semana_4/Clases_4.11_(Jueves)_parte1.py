3dae24d725b424fc22c5c5951ebbee8f0283c6b6
Rafa1911 <rafael1911.menendez@gmail.com>
2025-10-02 21:45:19 -0600

    Clase del Jueves 2 de Octubre de 2025

---
+++ Semana_4/Clases_4.11_(Jueves)_parte1.py
@@ -0,0 +1,46 @@
+"""ItemIngresar = input("Ingrese el item:  ") 
+print(f"El item ingresado es {ItemIngresar}") #String(str) = Cadena de caracteres (no es valor  o significado numerico)"""
+
+"""nombre = "Alvin"
+inicial = "J"
+apellido = "Portillo"
+NombreCompleto = nombre+inicial+apellido
+print(NombreCompleto)
+Nombre_Completo = nombre+ " "+inicial+" "+apellido
+print(Nombre_Completo)"""
+
+""""
+s1 = ""
+s2 = " "
+s3 = "A B C"
+s4 = "01234"
+print(len(s1))
+print(len(s2))
+print(len(s3)) #cadena de caracteres, arreglo (en S3 hay un largo de 5 caracteres)
+print(len(s4))"""
+
+"""my_string = "ABCDEFGHI"aa"""
+"""print(len(my_string))"""
+"""letra = int(input("Indique el indice de la letra a mostrar (del 0 al 4):  ")) #hay que poner "int" para que lo tome como entero y no como string
+print(my_string[letra])"""
+
+"""print(len(my_string)) #herramienta para saber cuantos elementos tiene la cadena de caracteres."""
+
+"""my_string = "ABCDEFGHI"
+
+print(my_string[1:5:50]) #Subsetting con rebanada (slice): Inicio= Indice Inclusivo, Fin= Indice Exclusivo, Paso = Tamaño de saltos 
+#Indice negativo es contar los elementos desde el fin de la secuencia: -1 es el ultimo y -2 es el penultimo (esto hace que el codigo es mas legible y consiso)
+"""
+"""my_string = "ABCDEFGHI"""
+
+"""print("O" not in my_string)"""
+
+"""print("AJP"*8)"""
+
+my_string = "ABCDEFCCC"
+
+"""print(max(my_string))""" #o min
+
+print(my_string.count("C"))
+
+#"my_string". = para hacer metodos 
