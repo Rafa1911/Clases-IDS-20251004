f47860d8bcaa042d1cd0edc95aa7f3a899d64d25
Rafa1911 <rafael1911.menendez@gmail.com>
2025-10-07 18:04:46 -0600

    Martes, 7 de octubre del 2025

---
+++ Semana_5/Clase_5.13_(Martes).py
@@ -0,0 +1,46 @@
+"""
+nombre = "Alvin"
+palabra = "RECONOCER"
+otra_palabra = "palabra"
+"""
+
+"""
+print(len(nombre))
+
+print(nombre[2])
+
+print(nombre[2:4]) #"Roma ni se conoce sin oro, ni se conoce sin amor"= Eg de Palindromo"""
+"""
+print(len(palabra))
+print(palabra[::-1])
+print(otra_palabra[::-1])"""
+
+####################################
+"""
+palabra= "romaniseconocesinoroniseconocesinamor"
+
+print(palabra==palabra[::-1])"""
+
+"""
+numero = 255
+print(len(numero))
+"""
+
+##################################
+"""
+mi_palabra = "Jocote"
+mi_palabra_mayuscula = mi_palabra.upper()
+print(mi_palabra_mayuscula)"""
+
+#Tupa es una secuencia de objwtoa de python, definidos dentro de parentesis y separados por comas y se recuperan por medio de corcheter (como un arreglo)
+
+### ejercicio de tuplas
+
+tupla1 = (1,12,255,1289,1289,60000,19047837844)
+lista1 = [1,12,255,1289,1289,600000,19047837844]
+print(len(tupla1))
+print(tupla1[3:])
+
+lista1[-1] = 3
+
+print(lista1)
