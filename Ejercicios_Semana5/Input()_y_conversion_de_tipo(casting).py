0b450fdc77fc2bb1db2c5d163e8ad3139730059c
Rafa1911 <rafael1911.menendez@gmail.com>
2025-10-21 16:46:05 -0600

    lol

---
+++ Ejercicios_Semana5/Input()_y_conversion_de_tipo(casting).py
@@ -0,0 +1,30 @@
+"""
+nombre_del_usuario = input("Ingrese su nombre: ")
+saludo_personalizado = f"Hola, {nombre_del_usuario}, 🙋‍♂️🫡"
+print(type(nombre_del_usuario))
+print(saludo_personalizado)"""
+
+"""edad_del_usuario = int(input("Digite su edad: "))
+edad_duplicada = edad_del_usuario * 2
+print(type(edad_del_usuario))
+print(edad_duplicada)"""
+
+"""dos_numeros1 = int(input("Digite el primer numero entero al azar: ")) 
+dos_numeros2 = int(input("Digite el segundo numero entero al azar "))  
+suma_numero = dos_numeros1 + dos_numeros2     
+ 
+print(suma_numero)"""
+
+"""Numero_Decimal = float(input("Escriba un numero decimal al azar: "))
+Decimal_Mitad = Numero_Decimal / 2
+print(Decimal_Mitad)"""
+
+"""edad_usuario1 = int(input("Escriba su año de nacimiento: "))
+resultado_edad = f"Su edad es {2025 - edad_usuario1}"
+print(resultado_edad)"""
+
+precio = "Digite el precio del producto: "
+unidades = "Digite la cantidad de unidades que lleva del producto: "
+cantidado_pago = precio * unidades
+mensaje1 = ("Su precio a pagar es {cantidado_pago:.2f}")
+print(mensaje1)
---
+++ Ejercicios_Semana5/Variables,comentarios_y_print().py
@@ -0,0 +1,22 @@
+nombre = "Rafael Menendez"
+print(nombre)
+
+Edad = "Rafael Menendez de 19 años"
+Ciudad = "Vive en Antiguo Cuscatlan"
+Profesion = "Y actualmente es un estudiante en la ESEN" 
+Oracion_final1 = Edad + " " + Ciudad + " " + Profesion 
+print(Oracion_final1)
+
+# Una variable es una etiqueta para almacenar y referenicar datos, como nombres, numero, lugares, etc (Aprenderse esto) 
+
+año_actual = 2025 
+año_nacmiento = 2005
+Edad1 = año_actual - año_nacmiento 
+print(Edad1)
+
+pi = 3.141592655359 
+mensaje1 = f"El valor aproximado de pi es {pi:.4f}" 
+print(mensaje1)
+
+#La diferencia de una variable tipo entero y tipo cadena es que una almacena datos de numero enteros sin decimal y la otra almacena secuencias de caracteres que forman un texto (aprenderse esto)
+
