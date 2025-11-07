eb74b06fa27730f82615dc1b12819a9673738a40
Rafa1911 <rafael1911.menendez@gmail.com>
2025-11-06 18:26:56 -0600

    Update Ejericio_clase2.py

--- Semana_9/Ejericio_clase2.py
+++ Semana_9/Ejericio_clase2.py
@@ -3,15 +3,16 @@ def dui_validacion():
     condiciones = 0
     dui = input("Ingrese su dui: ")
     
-    guion = dui.split("-")
     
     if len(dui) == 10: 
-        condiciones + 1
+        condiciones += 1
    
     if dui.count("-") == 1:
-        condiciones + 1 
+        condiciones += 1 
         
-    if len(guion[-1]) == 1:
-        condiciones + 1
-    
-print
+        partes = dui.split("-")
+        
+        if len(partes[-1]) == 1:
+         condiciones += 1
+    print(f"cumple {condiciones} de condiciones")
+dui_validacion()
